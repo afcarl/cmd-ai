bdb82cb0648f5c8f17f7408ebf50c83e84fd569c
Smitha Milli <smitha.milli@gmail.com>
2014-10-20 14:00:11 -0700

    Initial commit

---
+++ cmdai/cmd.py
@@ -0,0 +1,35 @@
+def process_input(user_input):
+    """
+    Takes user input as a string & returns the result of the action requested.
+
+    Parameters:
+       user_input (string): user's input
+    """
+    pass
+
+
+def match_to_action(user_input):
+    """
+    Takes user input & returns the matching action.
+
+    Parameters:
+        user_input (string): user's input
+    Returns:
+        action (function): the action function
+    Throws:
+        MathcException: exception thrown when a match cannot be found
+    """
+    pass
+
+
+def ls(dir_to_list):
+    """
+    Lists the files and directories in the given directory
+
+    Parameters:
+        dir_to_list (string): Path from current directory to directory to list
+        files / directories from
+    Returns:
+        listing (list): A list of the files and directories in dir_to_list
+    """
+    pass
---
+++ test/test_cmd.py
@@ -0,0 +1,11 @@
+"""Tests for cmd.py"""
+import unittest
+from cmdai import *
+
+
+class CmdTest(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
